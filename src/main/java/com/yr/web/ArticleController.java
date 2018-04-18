8dfde9a1545635470bc74e4f1fa424fe87a942dd
yurui <yurui@192.168.8.178>
2018-04-18 10:23:44 +0800

    首页第一版

--- src/main/java/com/yr/web/ArticleController.java
+++ src/main/java/com/yr/web/ArticleController.java
@@ -6,6 +6,11 @@
  */
 package com.yr.web;
 
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,10 +28,24 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/article")
 public class ArticleController {
     /**
+     * 文章列表
+     */
+    private Set<String> articleIdList = new TreeSet<String>();
+    
+    @PostConstruct
+    public void init() {
+        articleIdList.add("20180417");
+        articleIdList.add("20180418");
+    }
+
+    /**
      * 文章页
      */
     @RequestMapping("/{articleId}")
     public ModelAndView article(@PathVariable("articleId") String articleId) {
+        if (!articleIdList.contains(articleId)) {
+            return new ModelAndView("error/404");
+        }
         return new ModelAndView("article/" + articleId);
     }
 }
---
+++ src/main/webapp/WEB-INF/ftl/error/404.ftl
@@ -0,0 +1,14 @@
+<#assign path="${request.getContextPath()}">
+<html>
+<head>
+    <title>鱼一一的小屋</title>
+    <link rel="stylesheet" href="${path}/css/main.css" type="text/css"/>
+</head>
+<body>
+    <#include "../common/header.ftl"/>
+    <div class="content">
+        <h1>404 你寻找的页面不在屋里</h1>
+    </div>
+    <#include "../common/footer.ftl"/>
+</body>
+</html>
